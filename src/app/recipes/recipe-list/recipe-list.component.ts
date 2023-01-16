c7ff85c49c11ffede0c9ca5c1f1f63e8c62e7ae3
Iyan Theekshana <iyan.dev123@gmail.com>
2023-01-16 12:56:49 +0100

    added recipe and shoppinglist services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Pizza',
-      'This is an italian pizza',
-      'https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    ),
-    new Recipe(
-      'Pizza 2',
-      'This is an italian pizza 2',
-      'https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    ),
-  ];
+  recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipe();
+  }
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
   }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({ providedIn: 'root' })
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Pizza',
+      'This is an italian pizza',
+      'https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    ),
+    new Recipe(
+      'Pizza 2',
+      'This is an italian pizza 2',
+      'https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    ),
+  ];
+
+  getRecipe() {
+    return this.recipes.slice();
+  }
+}
